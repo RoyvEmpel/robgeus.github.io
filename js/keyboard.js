57a3b11b4db71b51be0bbf797355bd093ca26176
robgeus <36758721+robgeus@users.noreply.github.com>
2019-01-10 20:12:00 +0100

    aewfinocKS

---
+++ js/keyboard.js
@@ -0,0 +1,3 @@
+function input(){
+    document.write("done");
+}
--- js/typing.js
+++ js/typing.js
@@ -1,39 +1,92 @@
 var i = 0;
-var txt = 'MSDOS: ';
-
+var x = 0;
 var y = 0;
-var txt2 = 'Welcome to a test. ';
+var k = 0;
 
-var o = 0;
-var txt3 = 'This is a test.';
-
+var txt = 'MSDOS: ';
+var welcometxt = 'Welcome to my website!';
+var welcometxt2 = 'This is the place where I will tell your what I learned at my college.';
+var welcometxt3 = 'So lets go and look at my progress.';
 var speed = 75;
+var idlex = 0;
+var idle_1 = 'MSDOS:_'
+var idle_2 = 'MSDOS: '
 
-tekst
+var knipperspeed = 400;
+
+var test = true;
+var welcome_2_0 = true;
+var welcome_3_0 = true;
+
+
 
 function startup() {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+  if (i < txt.length) {
+    document.getElementById("dos").innerHTML += txt.charAt(i);
     i++;
-    setTimeout(startup, 100);
-    setTimeout(idle, 2000);
-    idling = true;
+    setTimeout(startup, speed);
+    
+  }else if (i = txt.length) {
+      idle();
+      setTimeout(welcome, 4000);
+  }
+  
 }
 
 function idle() {
-    if(idling == true){
-        document.getElementById("demo").innerHTML = 'MSDOS: _';
-        setTimeout(idle, 1000);
-        document.getElementById("demo").innerHTML = 'MSDOS:  ';
-        setTimeout(idle, 1000);
-        idle();
+    if (test == true){
+        document.getElementById("dos").innerHTML = idle_1;
+        setTimeout(idle2, knipperspeed);
+
+    }else if(idlex > 0){
+        document.getElementById("dos").innerHTML = txt;
+        idlex++;
+    }
+    
+}
+
+function idle2(){
+    if (test == true){
+        document.getElementById("dos").innerHTML = idle_2;
+        setTimeout(idle, knipperspeed);
     }
 }
 
-function lineodn() {
-  if (o < txt3.length) {
-    document.getElementById("content").innerHTML += txt3.charAt(o);
-    o++;
-    setTimeout(lineodn, 170);
-  }
+function welcome(){
+    test = false;
+    if (x < welcometxt.length) {
+        document.getElementById("dos").innerHTML += welcometxt.charAt(x);
+        x++;
+        setTimeout(welcome, speed);
+
+    }else{
+        setTimeout(welcome_2, 1000);
+    } 
 }
 
+function welcome_2(){
+    if (welcome_2_0 == true){
+        document.getElementById("dos").innerHTML = txt;
+        welcome_2_0 = false;
+        welcome_2();
+    }else if (y < welcometxt2.length) {
+        document.getElementById("dos").innerHTML += welcometxt2.charAt(y);
+        y++;
+        setTimeout(welcome_2, speed);
+    }else{
+        setTimeout(welcome_3, 1000);
+    } 
+    
+}
+ 
+function welcome_3(){
+    if (welcome_3_0 == true){
+        document.getElementById("dos").innerHTML = txt;
+        welcome_3_0 = false;
+        welcome_3();
+    }else if (k < welcometxt3.length) {
+        document.getElementById("dos").innerHTML += welcometxt3.charAt(k);
+        k++;
+        setTimeout(welcome_3, speed);
+    }
+}
